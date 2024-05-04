7b8b7305c82d0244de26c07d460207cbb901d188
osama0010 <osama.00.mail@gmail.com>
2024-05-04 23:40:38 +0300

    DAL for Department model

---
+++ Route.C41.G0P.DAL/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41.G0P.DAL.Data.Configurations;
+using Route.C41.G0P.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G0P.DAL.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+            => optionsBuilder.UseSqlServer("Server = . ; Database = MVCApplication; Trusted_connection = True; MultipleActiveResultSets = False;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Department { get; set; }
+
+    }
+}
---
+++ Route.C41.G0P.DAL/Data/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Route.C41.G0P.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G0P.DAL.Data.Configurations
+{
+    internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            //Fluent API for Department model
+            builder.Property(d => d.Id).UseIdentityColumn(10, 10);
+            builder.Property(d => d.Name).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+            builder.Property(d => d.Code).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+
+        }
+    }
+}
